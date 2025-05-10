748eac0ce458f8b035aed0dd08633fe6637c669f
IlyassaPutra-png <ilyasputra6071@domain.com>
2025-05-10 13:01:59 +0700

    setting

--- main_menu.gd
+++ main_menu.gd
@@ -1,4 +1,13 @@
 extends Control
+@onready var main_button: VBoxContainer = $MainButton
+@onready var options: Panel = $Options
+
+func _process(delta):
+	pass
+
+func _ready():
+	main_button.visible = true
+	options.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
@@ -6,7 +15,13 @@ func _on_start_pressed() -> void:
 
 func _on_setting_pressed() -> void:
 	print ("Setting_pressed")
+	main_button.visible = false
+	options.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
