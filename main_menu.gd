8c0dbb82b6b8831fe28c639ff409f824d04ba1ad
Baalrmdhn <muhamadiqbalramadhan363@gmail.com>
2025-05-11 17:49:45 +0700

    main menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,32 +1,17 @@
 extends Control
 @onready var main_button: VBoxContainer = $MainButton
 @onready var options: Panel = $Options
-@onready var mulai_menu: Panel = $Mulai_menu
-@onready var line_edit = get_node("Mulai_menu/TextureRect/LineEdit")
-
-func _Mulai():
-	line_edit.grab_focus()
-
-func _on_mulai_pressed():
-	Global.username = line_edit.text
-	get_tree().change_scene_to_file("res://game.tscn")
-
 
 func _process(delta):
 	pass
-
-func _readi():
-	main_button.visible = true
-	mulai_menu.visible = false
 
 func _ready():
 	main_button.visible = true
 	options.visible = false
 
 func _on_start_pressed() -> void:
-	print ("Login_pressed")
-	main_button.visible = false
-	mulai_menu.visible = true
+	get_tree().change_scene_to_file("res://game.tscn")
+
 
 func _on_setting_pressed() -> void:
 	print ("Setting_pressed")
@@ -40,14 +25,7 @@ func _on_exit_pressed() -> void:
 
 func _on_back_options_pressed() -> void:
 	_ready()
-<<<<<<< HEAD
 
 
 func _on_music_value_changed(value: float) -> void:
 	pass # Replace with function body.
-=======
-	
-
-func _on_back_pressed() -> void:
-	_readi()
->>>>>>> 1a70257dc7e051f08009b12d79c9a0d1a1152b04
